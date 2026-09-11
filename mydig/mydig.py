de37c9737a4c4922465a0614c58c765d989896f9
stjchang <changstephen289@gmail.com>
2026-09-11 15:28:34 -0400

    feat(root_servers): added root server list. refactor(main): refactored structure into main for future purposes

--- mydig/mydig.py
+++ mydig/mydig.py
@@ -1,9 +1,22 @@
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python mydig.py <domain>")
-    sys.exit(1)
-    
-domain = sys.argv[1]
+ROOT_SERVERS = [
+    "198.41.0.4"
+]
 
-print(f"Resolving: {domain}")
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python mydig.py <domain>")
+        sys.exit(1)
+
+    domain = sys.argv[1].rstrip(".")
+
+    print(f"Resolving: {domain}")
+
+    root_server = ROOT_SERVERS[0]
+
+    print(f"Starting DNS server: {root_server}")
+
+
+if __name__ == "__main__":
+    main()
